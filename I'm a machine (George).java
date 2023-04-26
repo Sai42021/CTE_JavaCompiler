e7df347a6e795fda319fa509401a722f124fb89f
georgediyeve <83222104+georgediyeve@users.noreply.github.com>
2023-04-26 17:35:21 +0200

    I'm a machine (George).java

--- I'm a machine (George).java
+++ I'm a machine (George).java
@@ -5,32 +5,15 @@ public class targetMachineCode {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter number of strings: ");
-        int strNums = in.nextInt();
-        in.nextLine();
+        System.out.println("Enter a string of letters: ");
+        String letters = in.nextLine();
 
-        String str = null;
-        int i = 0;
-
-        /* Strings loop
-        for (i = 1; i <= strNums; i++) {
-            System.out.println("\nEnter string #" + i + ":");
-            str = in.nextLine();*/
-
-        System.out.println("Enter a letter:");
-        String letter = in.nextLine();
-
-        if (letter.length() == 1 && Character.isLetter(letter.charAt(0))) {
-            char c = letter.charAt(0);
-            if (Character.isUpperCase(c) || Character.isLowerCase(c)) {
+        for (int i = 0; i < letters.length(); i++) {
+            char c = letters.charAt(i);
+            if (Character.isLetter(c)) {
                 String binary = Integer.toBinaryString(c);
-                System.out.printf("The binary form of %s is %s.\n", letter, binary);
-            } else {
-                System.out.println("Error: input is not a letter.");
+                System.out.printf(binary + " ");
             }
-        } else {
-            System.out.println("Error: input is not a single letter.");
         }
     }
-
 }
