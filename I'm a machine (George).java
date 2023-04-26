29e17ba91551a1a3833f9a4c85e7c2784fc4f245
georgediyeve <83222104+georgediyeve@users.noreply.github.com>
2023-04-26 23:23:45 +0200

    I'm a machine (George).java

--- I'm a machine (George).java
+++ I'm a machine (George).java
@@ -5,6 +5,10 @@ public class targetMachineCode {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("======STAGE7: TARGET MACHINE CODE ");
+
+
+
         System.out.println("Enter a string of letters: ");
         String letters = in.nextLine();
 
@@ -13,6 +17,16 @@ public class targetMachineCode {
             if (Character.isLetter(c)) {
                 String binary = Integer.toBinaryString(c);
                 System.out.printf(binary + " ");
+
+                if(letters.contains("DIV")){
+                    System.out.println("01000100");
+                }else if(letters.contains("MUL")){
+                    System.out.println("01001101");
+                }else if (letters.contains("ADD")){
+                    System.out.println("01000001");
+                }else if (letters.contains("SUB")){
+                    System.out.println("01010011");
+                }
             }
         }
     }
